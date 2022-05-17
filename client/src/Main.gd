01fc9148dc2e27e3c89beaf7dbe5286a24fd2321
Charles Micou <charlesmicou@gmail.com>
2022-05-17 17:26:44 +0100

    Add placeholder otter assets

--- client/src/Main.gd
+++ client/src/Main.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
 func _ready():
-	pass
+	$Otter1.apply_impulse(Vector2(0.0, 0.0), Vector2(50.0, 30.0))
+	$Otter2.apply_impulse(Vector2(0.0, 0.0), Vector2(-20.0, 70.0))
 
 func _process(delta):
-	$Sprite.rotate(0.1)
+	pass
